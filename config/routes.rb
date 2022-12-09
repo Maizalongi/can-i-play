104eaf429f61346101fded2a7067f263a6ff0087
Tomás Maiza <111014759+Maizalongi@users.noreply.github.com>
2022-12-08 21:36:00 -0300

    Revert "Rents controller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :rents
+  get 'rents/new'
+  get 'rents/create'
+  get 'rents/my_rents'
+  get 'rents/edit'
+  get 'rents/update'
+  get 'rents/destroy'
+  get 'rents/index'
+  get 'rents/show'
+  get 'games/index'
+  get 'games/show'
+  get 'games/new'
+  get 'games/create'
   get 'games/my_games'
-  get 'rents/:id/accept', to: 'rents#accept', as: :rent_accept
-  get 'rents/:id/deactivate', to: 'rents#deactivate', as: :rent_deactivate
+  get 'games/edit'
+  get 'games/update'
+  get 'games/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

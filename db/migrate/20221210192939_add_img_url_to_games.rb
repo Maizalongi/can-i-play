680150416eed6cff7368c9db7152c89f5feb9a32
Hernan Macaya <hjmacaya@uc.cl>
2022-12-17 12:39:27 -0300

    adding default poster

--- db/migrate/20221210192939_add_img_url_to_games.rb
+++ db/migrate/20221210192939_add_img_url_to_games.rb
@@ -1,5 +1,5 @@
 class AddImgUrlToGames < ActiveRecord::Migration[7.0]
   def change
-    add_column :games, :img_url, :string
+    add_column :games, :img_url, :string, default: "default_movie_poster.jpg"
   end
 end
